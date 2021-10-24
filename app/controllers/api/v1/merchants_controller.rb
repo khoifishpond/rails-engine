17338ca539d7735a3042c2830d612bc4666079de
Khoi Nguyen <khoinguyen311@gmail.com>
2021-10-24 16:17:24 -0700

    Create merchant files

---
+++ app/controllers/api/v1/merchants_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::MerchantsController < ApplicationController
+  def index
+    merchants = Merchant.paginate(page = params[:page].to_i, per_page = params[:per_page].to_i)
+    render json: MerchantSerializer.new(merchants)
+  end
+
+  def show
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant)
+  end
+end
